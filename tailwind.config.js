f83d434540404f233a5589572e4125e0274c4524
xrishi20 <xrishi20@gmail.com>
2020-10-22 19:45:56 +0530

    yoinked it

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
   purge: [],
   theme: {
     extend: {},
   },
   variants: {},
   plugins: [],
-  future: {
-    removeDeprecatedGapUtilities: true,
-  },
-};
+}
