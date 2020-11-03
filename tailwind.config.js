ae64bcf78534f29b651400453dcccccd236d0cc8
xrishi20 <xrishi20@gmail.com>
2020-11-03 15:49:36 +0530

    vault & vault Dashboard refinements

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,22 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: {
+    content: [
+      "./deleteModal.html",
+      "./discardModal.html",
+      "./index.html",
+      "./login.html",
+      "./recentActivity.html",
+      "./vaults.html",
+      "./vaultDashboard.html",
+      "./vModal.html",
+      "./vUModal.html",
+    ],
+  },
   theme: {
     extend: {},
   },
   variants: {},
   plugins: [],
-}
+};
